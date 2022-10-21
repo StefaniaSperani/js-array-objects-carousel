9b5a1f9a00fa90c33b78cd66ff6a8cb8d39e6100
StefaniaSperani <thedarkcry@hotmail.it>
2022-10-21 16:03:00 +0200

    added buttons

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ Dato un array di oggetti letterali con:
  - url dell’immagine
  - titolo
  - descrizione
+ 
 Creare un carosello come nella foto allegata.
 Milestone 0:
 Come sempre focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
